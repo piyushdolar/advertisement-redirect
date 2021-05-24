13ca6667dab1e148dcc015c59fb3c45bdf296831
Piyush Dolar <piyushdolar111@gmail.com>
2021-05-24 12:26:44 +0700

    eco updated to 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			name: 'advertisement-redirect',
 			port: 3000,
 			exec_mode: 'cluster',
-			instances: 'max', // Or a number of instances
+			instances: '1', // Or a number of instances
 			script: './node_modules/nuxt/bin/nuxt.js',
 			args: 'start',
 		}
